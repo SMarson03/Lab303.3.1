c673c4959009f167767664c3035acfa697f19c87
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-07-01 09:34:49 -0400

    2

---
+++ src/IsEmptyExample.java
@@ -0,0 +1,9 @@
+public class IsEmptyExample {
+    public static void main(String args[]){
+        String s1 = "";
+        String s2 = "hello";
+        System.out.println(s1.isEmpty());
+        System.out.println(s2.isEmpty());
+    }
+}
+//Why did the syntax change from (String[] args) to (String args[])?
