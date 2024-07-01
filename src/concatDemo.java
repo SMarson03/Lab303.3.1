5c5f8321a1743a561c69ab5155e6a86f6ae053bd
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-07-01 09:46:20 -0400

    4

---
+++ src/concatDemo.java
@@ -0,0 +1,8 @@
+public class concatDemo {
+    public static void main(String[] args){
+        String str1 = "Learn";
+        String str2 = "Java";
+
+        System.out.println(str1.concat(str2));
+    }
+}
